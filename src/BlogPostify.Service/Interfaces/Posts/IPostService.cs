49bf691557f9566ad3e813607d516c5d55bfda2b
xushnudbek <154055594+khushnudbekholbutaev@users.noreply.github.com>
2025-04-01 23:15:31 +0500

    Update IPostService.cs

--- src/BlogPostify.Service/Interfaces/Posts/IPostService.cs
+++ src/BlogPostify.Service/Interfaces/Posts/IPostService.cs
@@ -9,7 +9,7 @@ public interface IPostService
     Task<PostForResultDto> RetrieveIdAsync(int id);
     Task<PostForResultDto> AddAsync(PostForCreationDto dto);
     Task<PostForResultDto> ModifyAsync(int id, PostForUpdateDto dto);
-    Task<List<LanguageResultDto>> RetrieveByLanguageAsync(string language);
+    Task<List<LanguageResultDto>> RetrieveByLanguageAsync(string language, string tag);
     Task<List<PostTitleDto>> SearchByTagAsync(string tag, string language);
 
 }
